13f49fd1a28c31a49abc0990ee8d76e5afed4e4f
htekwani1 <htekwani1@gmail.com>
2023-02-09 14:40:44 -0700

    outline

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-// function parseSurvey(){
+/* 
+script for photography-app
+Version 1.0.0
+Authors: Big Data Dynasty group CIS 440
 
 
-// }
+var firstName = null
+var lastName = null
+var email = null
+var username = null
+var password = null
+var photoStyle = null
+var photoType = null
+var custAvailability = null
+var custBudget = null
+var 
 
-// function sendEmail(){
+function parseSurvey(){
+    
+
+ }
+
+ function sendEmail(){
 
 
-// }
+ }
+
+ */
