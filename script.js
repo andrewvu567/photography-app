0cacb30f2f23d5c905d59a0b9186681beec9a3ee
MatthewCKwan <94721669+MatthewCKwan@users.noreply.github.com>
2023-02-10 15:55:04 -0700

    adding survey, script, and styles

--- script.js
+++ script.js
@@ -2,11 +2,8 @@
 script for photography-app
 Version 1.0.0
 Authors: Big Data Dynasty group CIS 440
-
 TEST TEST 
-
 git push and commit test
-
 var firstName = null
 var lastName = null
 var email = null
@@ -17,15 +14,34 @@ var photoType = null
 var custAvailability = null
 var custBudget = null
 var 
-
 function parseSurvey(){
     
-
  }
-
  function sendEmail(){
-
-
  }
-
  */
+
+function TestButtonHandler() {
+    var webMethod = "ProjectServices.asmx/TestConnection";
+    var parameters = "{}";
+
+    //jQuery ajax method
+    $.ajax({
+        type: "POST",
+        url: webMethod,
+        data: parameters,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (msg) {
+            var responseFromServer = msg.d;
+            alert(responseFromServer);
+        },
+        error: function (e) {
+            alert("this code will only execute if javascript is unable to access the webservice");
+        }
+    });
+}
+
+function redirectToIndex() {
+    window.location.href='index.html';
+}
